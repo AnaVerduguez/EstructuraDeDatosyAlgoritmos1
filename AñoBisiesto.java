401710033eae71c0adb93ff4d23d1f2414125c83
Ana Verduguez <64393490+AnaVerduguez@users.noreply.github.com>
2023-01-07 00:23:16 -0300

    Add files via upload

---
+++ AñoBisiesto.java
@@ -0,0 +1,14 @@
+public class correcion{ 
+    public static void main(String args[]){
+
+       System.out.println("Ingrese un año pasa saber si es bisiesto o no:");
+       año=objeto.nextInt( );
+
+       if (año % 4 == 0 && año % 400 == 0){
+            return false;
+
+       }else{
+            return true;
+       }
+   }
+}
